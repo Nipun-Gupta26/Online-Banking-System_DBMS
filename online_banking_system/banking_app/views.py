7dac980a95e2196b96870c332941ef446d03d112
Nipun-Gupta26 <nipungupta2602@gmail.com>
2022-04-23 10:43:33 +0530

    testing

--- online_banking_system/banking_app/views.py
+++ online_banking_system/banking_app/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import connection
 
 
 # Create your views here.
-def index(request) : 
-    return render(request,'index.html')
+def index(request):
+    context ={}
+    with connection.cursor() as cursor:
+        query = "select * from test where age = 20"
+        cursor.execute(query)
+        row = cursor.fetchall()
+        print(row)
+        context = {
+            'name': row[0][0],
+            'age': row[0][1],
+        }
+
+    return render(request, 'index.html',context)
