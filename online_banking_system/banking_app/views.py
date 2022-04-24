c3115f54d74d3dd08dde8903c038efc266362d44
atharva20038 <atharva20038@iiitd.ac.in>
2022-04-24 16:01:26 +0530

    index

--- online_banking_system/banking_app/views.py
+++ online_banking_system/banking_app/views.py
@@ -6,20 +6,20 @@ import json
 
 # Create your views here.
 def index(request):
-    context ={}
-    with connection.cursor() as cursor:
-        query = "select * from test where age = 20"
-        cursor.execute(query)
-        row = cursor.fetchall()
-        print(row)
-        l = []
-        for x in row:
-            temp = []
-            for y in x:
-                temp.append(y)
-            l.append(temp)
-        context = {
-            'id': l
-        }
+    # context ={}
+    # with connection.cursor() as cursor:
+    #     query = "select * from test where age = 20"
+    #     cursor.execute(query)
+    #     row = cursor.fetchall()
+    #     print(row)
+    #     l = []
+    #     for x in row:
+    #         temp = []
+    #         for y in x:
+    #             temp.append(y)
+    #         l.append(temp)
+    #     context = {
+    #         'id': l
+    #     }
 
-    return render(request, 'index.html',context)
+    return render(request, 'index.html')
