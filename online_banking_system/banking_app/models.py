f5ff05ac154e2638f11c30200cc977b88261174a
atharva20038 <atharva20038@iiitd.ac.in>
2022-04-24 14:08:38 +0530

    updating nav bar

--- online_banking_system/banking_app/models.py
+++ online_banking_system/banking_app/models.py
@@ -1,3 +1,10 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Account : 
+    accNumber = models.IntegerField()
+    balance = models.IntegerField()
+    category = models.CharField(max_length=45)
+    customerID = models.IntegerField()
+    branchID = models.IntegerField()
