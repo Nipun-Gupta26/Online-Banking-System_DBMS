fbde481ba7fdf6e4e195e65dad446e76a051b9d0
atharva20038 <atharva20038@iiitd.ac.in>
2022-04-25 17:15:43 +0530

    ui

--- online_banking_system/banking_app/views.py
+++ online_banking_system/banking_app/views.py
@@ -119,7 +119,7 @@ def sign_out(request):
 def make_account(request):
     with connection.cursor() as cursor:
         
-        query1 = "select count(*) from verifies (where customerID in (select customerID from customer where userID = {})) and isVerified = {}".format(user.userID, 1)
+        query1 = "select count(*) from verifies where isVerified = {} and customerID in (select customerID from customer where userID = {}))".format(1, user.userID)
         cursor.execute(query1)
         result = cursor.fetchall()
         
@@ -135,4 +135,7 @@ def make_account(request):
             branchID = result[randint(0, len(result) - 1)][0]
             query4 = "insert into account values ({}, {}, {}, {})".format(acc_num, bal, accType, branchID)
             cursor.execute(query4)
-        return 
+        return
+
+def account(request) :
+    return  render(request, 'make_account.html') 
