c4788520a8aeceae80fdf9f1b92d59617b69a965
Nipun-Gupta26 <nipungupta2602@gmail.com>
2022-04-26 15:03:39 +0530

    bug fix

--- online_banking_system/banking_app/utils.py
+++ online_banking_system/banking_app/utils.py
@@ -4,7 +4,7 @@ def cust_views(customerID):
     with connection.cursor() as cursor:
         query1 = "create view accounts as select * from account where accNumber in (select accNumber from hasAccount where customerID={})".format(customerID)
         query2 = "create view transactions as select * from transaction where (customerCredited = {} or customerDebited = {})".format(customerID, customerID)
-        query3 = "create view loans as select * from loan where and isVerified = {} and loanID in (select loanID from borrows where customerID = {})".format(1, customerID)
+        query3 = "create view loans as select * from loan where isVerified = {} and loanID in (select loanID from borrows where customerID = {})".format(1, customerID)
         cursor.execute(query1)
         cursor.execute(query2)
         cursor.execute(query3)
