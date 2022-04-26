f109b7748a3f7371c6a7830314727a07b5111668
atharva20038 <atharva20038@iiitd.ac.in>
2022-04-26 15:00:50 +0530

    sql error sort

--- online_banking_system/banking_app/utils.py
+++ online_banking_system/banking_app/utils.py
@@ -4,7 +4,7 @@ def cust_views(customerID):
     with connection.cursor() as cursor:
         query1 = "create view accounts as select * from account where accNumber in (select accNumber from hasAccount where customerID={})".format(customerID)
         query2 = "create view transactions as select * from transaction where (customerCredited = {} or customerDebited = {})".format(customerID, customerID)
-        query3 = "create view loans as select * from loan where customerID = {} and is verified = {}".format(customerID, 1)
+        query3 = "create view loans as select * from loan where loanID in(select loanID from borrcustomerID = {}) and isVerified = {}".format(customerID, 1)
         cursor.execute(query1)
         cursor.execute(query2)
         cursor.execute(query3)
